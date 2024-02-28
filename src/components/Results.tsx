b13a187ca4cb577089cae6a19b43f715d6f1b051
Eoghan Hynes <eoghan@hyn.es>
2024-02-28 22:28:28 +0000

    Add unhappy state

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -4,18 +4,27 @@ import { Container, Row, Col } from "react-grid-system";
 const Results = ({ leftSwipes }: { leftSwipes: string[] }) => {
   return (
     <Container className="bit-card results">
-      <h2>You all agreed on:</h2>
-      <Row>
-          <Col md={4} />
-          <Col md={4}>
-            <ul>
-              {leftSwipes.map((swipe, idx) => (
-                <li key={`${swipe}-${idx}`}>{swipe}</li>
-              ))}
-            </ul>
-          </Col>
-          <Col md={4} />
-      </Row>
+      {leftSwipes.length ? (
+        <>
+          <h2>You all agreed on:</h2>
+          <Row>
+            <Col md={4} />
+            <Col md={4}>
+              <ul>
+                {leftSwipes.map((swipe, idx) => (
+                  <li key={`${swipe}-${idx}`}>{swipe}</li>
+                ))}
+              </ul>
+            </Col>
+            <Col md={4} />
+          </Row>
+        </>
+      ) : (
+        <>
+          <h2>You didn't agree on anything!</h2>
+          <h3>:(</h3>
+        </>
+      )}
     </Container>
   );
 };
